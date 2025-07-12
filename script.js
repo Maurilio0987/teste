afceb301c6f06eb464b5dc8a1d8035036de66895
__Maurilio__ <116044148+Maurilio0987@users.noreply.github.com>
2025-07-12 17:25:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const video = document.getElementById('video');
+const statusEl = document.getElementById('status');
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+]).then(startVideo);
+
+function startVideo() {
+  navigator.mediaDevices.getUserMedia({ video: {} })
+    .then(stream => {
+      video.srcObject = stream;
+      statusEl.innerText = 'Câmera iniciada.';
+    })
+    .catch(err => console.error(err));
+}
+
+video.addEventListener('play', async () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  const labeledDescriptors = await loadLabeledImages();
+  const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.6);
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
+      drawBox.draw(canvas);
+      statusEl.innerText = `Reconhecido: ${result.toString()}`;
+    });
+  }, 1000);
+});
+
+function loadLabeledImages() {
+  const labels = ['Paulo']; // nomes dos usuários cadastrados
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = [];
+      for (let i = 1; i <= 1; i++) {
+        const img = await faceapi.fetchImage(`/models/${label}.jpg`);
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
+}
